7ffed48dbb4af563ecce9f5ad9a5680177bfc06e
Agustin Salinas <chaucharian@gmail.com>
2019-01-14 16:40:32 -0300

    replace js by jsx files and eslint working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,45 @@
-module.exports = {
-    "extends": "google"
+module.exports = exports = {
+    // "env": {
+    //     "es6": true
+    // },
+    // "extends": "eslint:recommended",
+    // "parserOptions": {
+    //     "sourceType": "module"
+    // }
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "ecmaFeatures": {
+          "jsx": true
+        },
+        "sourceType": "module"
+      },
+    
+      "settings": {
+        "react": {
+          "version": "16.0"
+        }
+      },
+    
+      "plugins": [
+        "react"
+      ],
+    
+      "extends": [
+        "standard-jsx"
+      ],
+    
+      "rules": {
+        "jsx-quotes": ["warn", "prefer-double"],
+        "react/jsx-no-bind": ["error", {
+          "allowArrowFunctions": true,
+          "allowBind": false,
+          "ignoreRefs": true
+        }],
+        "react/no-did-update-set-state": "error",
+        "react/no-unknown-property": "error",
+        "react/no-unused-prop-types": "error",
+        "react/prop-types": "error",
+        "react/react-in-jsx-scope": "error"
+      }
+    
 };
