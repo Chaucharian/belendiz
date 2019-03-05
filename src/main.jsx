95dc7257bd29e68f42122a70a75f0730d1200913
Agustin Salinas <chaucharian@gmail.com>
2019-03-04 22:41:33 -0300

    change packagejson

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,33 @@ import SectionContainer from './components/sectionContainer.jsx';
 import Section from './components/section.jsx';
 import Background from './model/imageSections';
 
-const animationConfig = { transitionTime: 0.5, transitionType: 'linear', background: Background.default };
-const animationConfig2 = { transitionTime: 4, transitionType: 'ease', background: Background.default };
-const animationConfig3 = { transitionTime: 2, transitionType: 'ease', background: Background.default };
+const animationConfig1 = { transitionTime: 2, transitionType: 'ease-in-out', background: Background.one };
+const animationConfig2= { transitionTime: 2, transitionType: 'ease-in-out', background: Background.two };
+const animationConfig3 = { transitionTime: 2, transitionType: 'ease-in-out', background: Background.three };
+const animationConfig4 = { transitionTime: 2, transitionType: 'ease-in-out', background: Background.four };
+const animationConfig5 = { transitionTime: 2, transitionType: 'linear', background: Background.five };
+const animationConfig6 = { transitionTime: 2, transitionType: 'ease', background: Background.six };
+const animationConfig7 = { transitionTime: 2, transitionType: 'ease', background: Background.seven };
+const animationConfig8 = { transitionTime: 2, transitionType: 'ease', background: Background.eight };
+const animationConfig9 = { transitionTime: 2, transitionType: 'ease', background: Background.nine };
+const animationConfig10 = { transitionTime: 2, transitionType: 'ease', background: Background.twentysix };
+
 
 const Page = () => (
   <SlideWrapper>
-    <Section {...animationConfig2} />
-    <Section {...animationConfig2} />
+    <Section {...animationConfig1} />
+    <SectionContainer>
+      <Section {...animationConfig2} />
+      <Section {...animationConfig4} />
+      <Section {...animationConfig3} />
+    </SectionContainer>
+    <SectionContainer>
+      <Section {...animationConfig5} />
+      <Section {...animationConfig6} />
+    </SectionContainer>
+    <Section {...animationConfig8} />
+    <Section {...animationConfig9} />
+    <Section {...animationConfig10} />
   </SlideWrapper>
 );
 
--- src/model/imageSections.js
+++ src/model/imageSections.js
@@ -1,5 +1,46 @@
 import defaultImage from '../images/default.jpg';
+import one from '../images/1.jpg';
+import two from '../images/2.jpg';
+import three from '../images/3.jpg';
+import four from '../images/4.jpg';
+import five from '../images/5.jpg';
+import six from '../images/6.jpg';
+import seven from '../images/7.jpg';
+import eight from '../images/8.jpg';
+import nine from '../images/9.jpg';
+import ten from '../images/10.jpg';
+import eleven from '../images/11.jpg';
+import twelve from '../images/12.jpg';
+import threeten from '../images/13.jpg';
+import fourten from '../images/14.jpg';
+import fiveten from '../images/15.jpg';
+import sixten from '../images/16.jpg';
+import twentysix from '../images/26.jpg'
 
 export default {
-    "default" : defaultImage
+    "default" : defaultImage,
+    "one" : five,
+    "two" : one,
+    "three" : six,
+    "four" : seven,
+    "five" : eight,
+    "six" : nine,
+    "seven" : ten,
+    "eight" : eleven,
+    "nine" : twelve,
+    "ten" : ten,
+    "eleven" : eleven,
+    "twelve" : twelve,
+    "threeten" : threeten,
+    "fourten" : fourten,
+    "fiveten" : fiveten,
+    "sixten" : sixten,
+    "twentysix" : twentysix
+ /*   "17" : seventen,
+    "18" : eightten,
+    "19" : nineten,
+    "20" : twenty,
+    "21" : twentyone,
+    "22" : twentytwo,
+    "23" : twentythree,*/
 };
